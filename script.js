fa8234893d359112077eb940fb15cc18bb23dd8a
GarudaPS <152284636+GarudaPS@users.noreply.github.com>
2023-12-03 23:05:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
-yesBtn.addEventListener("click", ( ) => {
+yesBtn.addEventListener("click", () => {
   question.innerHTML = "Aaaaa, I like you too"
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
 
-noBtn.addEventListener("mouseover", ( ) => {
-  const noBtnRect = noBtn.getBoundingClientRect( );
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
   const maxX = window.innerWidth - noBtnRect.width;
-  const maxX = window.innerHeight - noBtnRect.height;
+  const maxY = window.innerHeight - noBtnRect.height;
 
-  const randomX = Math.floor(Math.random( ) * maxX);
-  const randomY = Math.floor(Math.random( ) * maxY);
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
 
   noBtn.style.left = randomX + "px";
-  noBrn.style.top = randmY + "px";
+  noBtn.style.top = randmY + "px";
 });
